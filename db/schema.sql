97793a9e39a5022575b0db47e5643d9379bb3a67
nh nc <nhnc11@yahoo.com>
2018-04-28 15:11:45 -0400

    final app

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,9 @@
-### Schema
-DROP DATABASE burgers_db;
-
-CREATE DATABASE burgers_db;
-USE burgers_db;
-
-
+CREATE TABLE todos (
+	id int AUTO_INCREMENT NOT NULL,
+	description varchar(255),
+	createdAt timestamp NOT NULL,
+	PRIMARY KEY(id));
+    
 CREATE TABLE burgers (
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(100),
